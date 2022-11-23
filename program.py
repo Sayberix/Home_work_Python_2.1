ce1b8115eca5276ff3795dc6d9180c39141ea0c0
Sayberix <mr.maaks@gmail.com>
2022-11-23 21:21:09 +0700

    задание выполнено

--- program.py
+++ program.py
@@ -2,3 +2,9 @@
 # ⋀ - and
 # ⋁ - or
 # ¬ - not
+
+for x in [True,False]:
+    for y in [True,False]:
+        for z in [True,False]:
+            resault = (not (x or y or z)) == ((not (x)) and (not (y)) and (not (z)))
+            print(f'¬({x} ⋁ {y} ⋁ {z}) = ¬{x} ⋀ ¬{y} ⋀ ¬{z} = {resault}')
